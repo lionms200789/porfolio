63bd410ce7a090c1a3e78992fbeb365c68202bae
chrtseng <chris_tseng@linkwish.com>
2023-08-21 16:48:32 +0800

    調整樣式與 skill area

--- app/components/Page/Home/Skills/Skills.jsx
+++ app/components/Page/Home/Skills/Skills.jsx
@@ -1,19 +1,9 @@
 'use client';
 
 import { useEffect } from 'react';
-import ReacGif from '@/images/icons/react.gif'
-import WorldWideGif from '@/images/icons/worldwide.gif'
-import PencilGif from '@/images/icons/pencil.gif';
 import { scrollTransition } from '@/utils/scrollTransition';
 
-const icons = {
-    frontEnd: ReacGif,
-    backEnd: WorldWideGif,
-    others: PencilGif
-};
-
 const Skills = ({ data = [] }) => {
-    console.log(data)
     useEffect(() => {
         if (data.length) {
             data.forEach(item => {
@@ -23,23 +13,18 @@ const Skills = ({ data = [] }) => {
         }
     }, [data]);
 
-    return <div className="container flex flex-col gap-7 md:gap-10">
+    return <div className="container flex flex-col gap-14 md:gap-20">
         {
             data.length ? data.map((skill, idx) => <div key={idx} className="md:mb-5">
                 <h4 className="text-lg md:text-xl font-bold mb-4 flex items-center">
-                    <img src={icons[skill.icon].src} className="w-8 lg:w-10 mr-2 mix-blend-darken" />
+                    <img src={skill.img} className="w-8 lg:w-10 mr-2 mix-blend-darken" />
                     <span>{skill.type}</span>
                 </h4>
-                <div className={`flex flex-col flex-wrap gap-1.5 ${skill.id}`}>
-                    {/* {
-                        skill.items.map((item, _idx) => <span key={_idx} className="px-5 rounded-full bg-cyan-600 text-white md:text-lg leading-[32px] md:leading-[36px]">
-                            {item.tag}
-                        </span>)
-                    } */}
+                <div className={`flex flex-col flex-wrap gap-4 md:gap-4 ${skill.id}`}>
                     {
-                        skill.items.map((item, idx) => <div key={idx} className='flex items-center'>
-                            <div>{item.tag}</div>
-                            <div>{item.description}</div>
+                        skill.items.map((item, idx) => <div key={idx} className={`flex items-center gap-3 md:gap-5 ${(idx % 2 === 0) ? 'flex-row-reverse' : ''}`}>
+                            <img title={item.tag} alt={item.description} src={item.img} className="w-8 md:w-14" />
+                            <div className={`relative bg-white p-3 rounded-lg drop-shadow ${idx % 2 === 0 ? 'triangle-r ml-[48px] md:ml-[72px]' : 'triangle-l mr-[48px] md:mr-[72px]'}`}>{item.description}</div>
                         </div>)
                     }
                 </div>
